56780f236d6b7b6857e590eef1bd41ffe0aff05e
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-12-19 18:13:31 -0300

    Fix bug visual

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { StackNavigator, DrawerNavigator } from "react-navigation";
+import { Animated, Easing } from 'react-native';
 import {
   LoginStack,
   HomeStack,
@@ -9,9 +10,20 @@ import {
   Drawer Stack 
   Aqui fica as cenas do side menu
 */
-const DrawerNav = DrawerNavigator({
-  home: { screen: HomeStack },
-  course: { screen: CourseStack }
+const DrawerNav = DrawerNavigator(
+  {
+    home: { screen: HomeStack },
+    course: { screen: CourseStack }
+  }
+);
+
+/* Corrige bug visual quando passa da loginStack -> drawerNav */
+const noTransitionConfig = () => ({
+  transitionSpec: {
+    duration: 0,
+    timing: Animated.timing,
+    easing: Easing.step0
+  }
 });
 
 /* Main Navigation */
@@ -22,7 +34,8 @@ const PrimaryNav = StackNavigator({
     // Default config for all screens
     headerMode: 'none',
     title: 'Main',
-    initialRouteName: 'drawerNav'
+    initialRouteName: 'loginStack',
+    transitionConfig: noTransitionConfig
   });
 
 export default PrimaryNav;
