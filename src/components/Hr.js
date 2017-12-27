b41e6cb371d9b3c028cec04f5347004529e52c80
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-12-27 14:56:42 -0300

    Atualiza style

--- src/components/Hr.js
+++ src/components/Hr.js
@@ -6,6 +6,8 @@ export default props => (
     style={{
       borderBottomColor: props.color || 'gray',
       borderBottomWidth: props.width || 0.5,
+      opacity: props.opacity || 0.62,
+      marginHorizontal: 10
     }}
   />
 );
