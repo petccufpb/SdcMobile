d7e2579299682f16724f4e46871d64dc2551695a
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2018-01-06 16:01:49 -0300

    Adiciona Tabs com datas para Tela de Programacao

--- src/navigation/stacks/HomeStack.js
+++ src/navigation/stacks/HomeStack.js
@@ -1,15 +1,28 @@
 /**
  * @description Stack das cenas pertencentes à navegação do Home
  */
-
+import React from "react";
 import { StackNavigator } from "react-navigation";
+import Icon from 'react-native-vector-icons/Entypo';
 
 import HomeScreen from "../../views/HomeScreen";
-import NavOptions from "./NavOptions";
+import { mainColor } from "../../util";
+
+const navOptions = ({ navigation }) => ({
+  title: 'Programação',
+  drawerLabel: 'Programação',
+  drawerIcon: ({focused, tintColor}) => (
+    <Icon name="calendar" size={20} color={focused ? mainColor : 'gray'}/>
+  ),
+  headerStyle: { backgroundColor: mainColor },
+  headerTintColor: 'white',
+  headerLeft: <Icon.Button name="menu" backgroundColor={mainColor} onPress={() =>
+    navigation.navigate('DrawerToggle')}/>
+});
 
 /* Controle de navegacao para o menu Home */
 export const HomeStack = StackNavigator({
   home: { screen: HomeScreen }
 }, {
-  navigationOptions: NavOptions
+  navigationOptions: navOptions
 });
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,30 +1,65 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import Icon from 'react-native-vector-icons/Entypo';
+import React, { Component } from "react";
+import { TabNavigator } from "react-navigation";
+import { View, Text, StyleSheet } from "react-native";
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import { mainColor } from '../util';
+import { mainColor } from "../util";
 
-export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  static navigationOptions = {
-    title: 'Programação',
-    drawerLabel: 'Programação',
-    drawerIcon: ({focused, tintColor}) => (
-      <Icon name="calendar" size={20} color={focused ? mainColor : 'gray'}/>
-    )
-  }
-
+class Monday extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> HomeScreen.js </Text>
+        <Text>Segunda</Text>
       </View>
     );
   }
 }
+
+class Tuesday extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Terça</Text>
+      </View>
+    );
+  }
+}
+
+class Wednesday extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Quarta</Text>
+      </View>
+    );
+  }
+}
+
+class Thursday extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Quinta</Text>
+      </View>
+    );
+  }
+}
+
+class Friday extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Sexta</Text>
+      </View>
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -33,3 +68,79 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   }
 });
+
+export default TabHome = TabNavigator({
+  Monday: {
+    screen: Monday,
+    navigationOptions: {
+      tabBarLabel: 'Segunda',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Entypo //Componente do icone da barra de navegação inferior
+          name="modern-mic"
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Tuesday: {
+    screen: Tuesday,
+    navigationOptions: {
+      tabBarLabel: 'Terça',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <MaterialCommunityIcons //Componente do icone da barra de navegação inferior
+          name="presentation"
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Wednesday: {
+    screen: Wednesday,
+    navigationOptions: {
+      tabBarLabel: 'Quarta',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Entypo //Componente do icone da barra de navegação inferior
+          name="medal"
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Thursday: {
+    screen: Thursday,
+    navigationOptions: {
+      tabBarLabel: 'Quinta',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Entypo //Componente do icone da barra de navegação inferior
+          name="code"
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+  Friday: {
+    screen: Friday,
+    navigationOptions: {
+      tabBarLabel: 'Sexta',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Entypo //Componente do icone da barra de navegação inferior
+          name="game-controller"
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    }
+  },
+}, {
+    tabBarPosition: 'top',
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarOptions: {
+      activeTintColor: mainColor,
+    },
+  }
+);
