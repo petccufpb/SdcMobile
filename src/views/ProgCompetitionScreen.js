a82e38cb90e916a0c736d8803cf9f7d299f1de20
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2018-01-17 14:29:26 -0300

    Finaliza tela

--- src/views/ProgCompetitionScreen.js
+++ src/views/ProgCompetitionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import { mainColor } from '../util';
@@ -20,7 +20,18 @@ export default class ProgCompetitionScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> ProgCompetitionScreen.js </Text>
+        <View style={styles.header}>
+          <Icon name="medal" size={120} color={mainColor}/>
+          <Text style={styles.title}>Maratona de Programação</Text>
+          <Text style={styles.subtitle}>Quarta - 07/02, às 8:00h</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.text}>{about}</Text>
+          <View style={styles.button}>
+            <Button color={mainColor} onPress={() => Linking.openURL(urlEdital)} title="Edital" />
+            <Button color={mainColor} onPress={() => Linking.openURL(urlForm)} title="Participar" />
+          </View>
+        </View>
       </View>
     );
   }
@@ -28,8 +39,42 @@ export default class ProgCompetitionScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1,    
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10
+  },
+  header: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  content: {
+    marginTop: 20,
+    marginHorizontal: 10
+  },
+  title: {
+    fontSize: 25,
+    color: mainColor,
+    fontWeight: 'bold'
+  },
+  subtitle: {
+    color: 'gray'
+  },
+  text: {
+    textAlign: 'justify',
+  },
+  button: {
+    marginTop: 30
   }
 });
+
+const urlEdital = "https://goo.gl/XskpVb";
+const urlForm = "https://goo.gl/forms/KkhgC9IKYpty41743";
+const about = "A maratona é um evento organizado pelo clube de maratona de programação "+
+"UFPB em conjunto ao PET.com, ele segue os moldes das maratonas e olimpíadas de "+
+"programação tradicionais e acontecerá dentro da Semana da Computação. A "+
+"maratona de programação da SDC tem como objetivo despertar nos alunos do Centro de "+
+"Informática o interesse por programação, para isso provas conta com problemas "+
+"desafiantes que estimulam a competição entre os alunos. O evento conta com dois níveis "+
+"de prova (iniciante e avançado) para tornar mais justa a competição entre todos os níveis "+
+"de alunos do CI.";
