82d9529eb5e7445c393dc2e58aaecc1aed0de66f
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2018-01-17 15:32:41 -0300

    Adiciona tela

--- src/views/GameDayScreen.js
+++ src/views/GameDayScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import { mainColor } from '../util';
@@ -12,16 +12,29 @@ export default class GameDayScreen extends Component {
   static navigationOptions = {
     title: 'Game Day',
     drawerLabel: 'Game Day',
-    drawerIcon: ({focused, tintColor}) => (
-      <Icon name="game-controller" size={20} color={focused ? mainColor : 'gray'}/>
+    drawerIcon: ({ focused, tintColor }) => (
+      <Icon name="game-controller" size={20} color={focused ? mainColor : 'gray'} />
     )
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text> GameDayScreen.js </Text>
-      </View>
+      <ScrollView style={{ backgroundColor: "white" }}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Icon name="game-controller" size={120} color={mainColor} />
+            <Text style={styles.title}>Game Day</Text>
+            <Text style={styles.subtitle}>Sexta - 09/02, às 8:00h</Text>
+            <Text style={styles.subtitle}>Local: </Text>
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.text}>{about}</Text>
+            <View style={styles.button}>
+              <Button color={mainColor} onPress={() => Linking.openURL(urlLol)} title="Participar do Campeonato X1 LOL" />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -29,7 +42,33 @@ export default class GameDayScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    padding: 10
+  },
+  header: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  content: {
+    marginTop: 20,
+    marginHorizontal: 10
+  },
+  title: {
+    fontSize: 25,
+    color: mainColor,
+    fontWeight: 'bold'
+  },
+  subtitle: {
+    color: 'gray'
+  },
+  text: {
+    textAlign: 'justify',
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 30
   }
 });
+const urlLol = "http://www.google.com";
+const about = "O game day é o último dia do evento, feito para relaxar e se divertir com a galera! " +
+  "O game day contará com muitas brincadeiras e jogos, além de alguns campeonatos dos jogos mais famosos! " +
+  "Para participar dos campeonatos é necessário realizar sua inscrição com antecedência.";
