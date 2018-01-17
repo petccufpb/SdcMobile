6ed19d833840f5bc5f8e17b2b3aff8b651b8d2e4
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2018-01-17 15:32:34 -0300

    Adiciona Scroll

--- src/views/ProgCompetitionScreen.js
+++ src/views/ProgCompetitionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Linking } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import { mainColor } from '../util';
@@ -12,34 +12,37 @@ export default class ProgCompetitionScreen extends Component {
   static navigationOptions = {
     title: 'Maratona',
     drawerLabel: 'Maratona de programação',
-    drawerIcon: ({focused, tintColor}) => (
-      <Icon name="medal" size={20} color={focused ? mainColor : 'gray'}/>
+    drawerIcon: ({ focused, tintColor }) => (
+      <Icon name="medal" size={20} color={focused ? mainColor : 'gray'} />
     )
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Icon name="medal" size={120} color={mainColor}/>
-          <Text style={styles.title}>Maratona de Programação</Text>
-          <Text style={styles.subtitle}>Quarta - 07/02, às 8:00h</Text>
-        </View>
-        <View style={styles.content}>
-          <Text style={styles.text}>{about}</Text>
-          <View style={styles.button}>
-            <Button color={mainColor} onPress={() => Linking.openURL(urlEdital)} title="Edital" />
-            <Button color={mainColor} onPress={() => Linking.openURL(urlForm)} title="Participar" />
+      <ScrollView style={{ backgroundColor: "white" }}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Icon name="medal" size={120} color={mainColor} />
+            <Text style={styles.title}>Maratona de Programação</Text>
+            <Text style={styles.subtitle}>Quarta - 07/02, às 8:00h</Text>
+            <Text style={styles.subtitle}>Local: </Text>
+          </View>
+          <View style={styles.content}>
+            <Text style={styles.text}>{about}</Text>
+            <View style={styles.button}>
+              <Button color={mainColor} onPress={() => Linking.openURL(urlEdital)} title="Edital" />
+              <Button color={mainColor} onPress={() => Linking.openURL(urlForm)} title="Participar" />
+            </View>
           </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,    
+    flex: 1,
     alignItems: 'center',
     backgroundColor: 'white',
     padding: 10
@@ -70,11 +73,11 @@ const styles = StyleSheet.create({
 
 const urlEdital = "https://goo.gl/XskpVb";
 const urlForm = "https://goo.gl/forms/KkhgC9IKYpty41743";
-const about = "A maratona é um evento organizado pelo clube de maratona de programação "+
-"UFPB em conjunto ao PET.com, ele segue os moldes das maratonas e olimpíadas de "+
-"programação tradicionais e acontecerá dentro da Semana da Computação. A "+
-"maratona de programação da SDC tem como objetivo despertar nos alunos do Centro de "+
-"Informática o interesse por programação, para isso provas conta com problemas "+
-"desafiantes que estimulam a competição entre os alunos. O evento conta com dois níveis "+
-"de prova (iniciante e avançado) para tornar mais justa a competição entre todos os níveis "+
-"de alunos do CI.";
+const about = "A maratona é um evento organizado pelo clube de maratona de programação " +
+  "UFPB em conjunto ao PET.com, ele segue os moldes das maratonas e olimpíadas de " +
+  "programação tradicionais e acontecerá dentro da Semana da Computação. A " +
+  "maratona de programação da SDC tem como objetivo despertar nos alunos do Centro de " +
+  "Informática o interesse por programação, para isso provas conta com problemas " +
+  "desafiantes que estimulam a competição entre os alunos. O evento conta com dois níveis " +
+  "de prova (iniciante e avançado) para tornar mais justa a competição entre todos os níveis " +
+  "de alunos do CI.";
