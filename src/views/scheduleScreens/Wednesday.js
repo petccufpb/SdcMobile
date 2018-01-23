db723fbc4355bdc6ddb8b6101eda0d218b2caff0
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2018-01-23 14:12:06 -0300

    Adiciona Quarta-feira

--- src/views/scheduleScreens/Wednesday.js
+++ src/views/scheduleScreens/Wednesday.js
@@ -1,22 +1,66 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet
-} from "react-native";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Text, FlatList } from "react-native";
 
-export default props => {
-  return (
-    <View style={styles.container}>
-      <Text>Quarta</Text>
-    </View>
-  );
+import {
+  Card,
+  ScheduleHeader,
+  ScheduleContent,
+  ScheduleItem,
+  ActivityIndicator
+} from "../../components/";
+
+import { getProgCompetition } from "../../actions/ProgCompetitionAction";
+
+class Wednesday extends Component {
+  constructor(props) {
+    super(props);
+    if (!this.props.progCompetition) {
+      this.props.getProgCompetition();
+    }
+  }
+
+  renderItem() {
+    return (
+      <ScheduleItem
+        iconName={this.props.progCompetition.icon}
+        title={this.props.progCompetition.title}
+        time={this.props.progCompetition.time}
+      />
+    );
+  }
+
+  renderActivityIndicator() {
+    return <ActivityIndicator />;
+  }
+
+  renderCard() {
+    if (this.props.progCompetition) {
+      return (
+        <Card>
+          <ScheduleHeader
+            iconName={this.props.progCompetition.icon}
+            title="Dia de Maratona"
+            subtitle="Quarta-feira 07/02"
+          />
+          <ScheduleContent>
+            {this.props.error ? <Text>{this.props.error}</Text> : this.renderItem()}
+          </ScheduleContent>
+        </Card>
+      );
+    }
+    return this.renderActivityIndicator();
+  }
+
+  render() {
+    return this.renderCard();
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+
+const mapStateToProps = state => ({
+  progCompetition: state.ProgCompetitionReducer.progCompetition,
+  loading: state.ProgCompetitionReducer.loading,
+  error: state.ProgCompetitionReducer.error,
 });
+export default connect(mapStateToProps, { getProgCompetition })(Wednesday);
