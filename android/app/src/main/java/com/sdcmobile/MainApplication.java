2f1f1a866ab5181eb53764c7df91987871723d14
Marcos Alves <alves.henrique.marcos@gmail.com>
2018-01-23 02:21:55 -0300

    Adiciona depencia LinearGradient + font oxygen

--- android/app/src/main/java/com/sdcmobile/MainApplication.java
+++ android/app/src/main/java/com/sdcmobile/MainApplication.java
@@ -3,6 +3,8 @@ package com.sdcmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.magus.fblogin.FacebookLoginPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FacebookLoginPackage(),
+            new LinearGradientPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage()
       );
